faba4d4752951c6e51855955ff14e3d5c9311d0d
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2025-01-23 16:34:01 -0600

    start of UI

---
+++ work-portfolio/src/components/header.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+export default function Header() {
+  return (
+
+    <div>
+        <nav>
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    </div>
+  )} 
