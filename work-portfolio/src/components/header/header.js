0dcfd91e490a246ff144e179ce0c11233656113b
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2025-02-15 11:21:16 -0600

    header js

---
+++ work-portfolio/src/components/header/header.js
@@ -0,0 +1,14 @@
+
+import './header.css'
+
+export default function Header() {
+  return (
+
+    <div class="header-container">
+        <nav class="header-content">
+            <ul>
+                <li>Work Portfolio</li>
+            </ul>
+        </nav>
+    </div>
+  )} 
