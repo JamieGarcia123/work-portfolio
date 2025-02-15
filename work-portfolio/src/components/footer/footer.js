ef4c30043c9bfb9346590b40ccb3ced0ddcb1ced
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2025-02-15 11:21:27 -0600

    footer files

---
+++ work-portfolio/src/components/footer/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+  
+      <footer class="footer-container">
+          <div class="footer-content">
+              Some content
+          </div>
+      </footer>
+    )} 
