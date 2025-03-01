29bf98e8276c7c40da3ecc7ad9c7b291d28ae12e
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2025-02-28 18:48:49 -0600

    start of projects page

---
+++ work-portfolio/src/pages/projects/index.js
@@ -0,0 +1,19 @@
+
+
+
+const Projects = () => {
+  return (
+    <div class="container" >
+        <section>
+          <h1>Projects
+          </h1>
+        </section>
+        
+    </div>
+  );
+}
+
+export default Projects;
+
+
+
