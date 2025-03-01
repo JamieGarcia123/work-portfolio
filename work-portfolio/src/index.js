dbfd66aa7da43204617e8a2391a0613d0a13845e
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2025-02-28 18:48:12 -0600

    routes added to main index.js

--- work-portfolio/src/index.js
+++ work-portfolio/src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Projects from './pages/projects/index';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router";
+import EducationPage  from "./pages/education/index";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} >
+      <Route  path="/projects" element={<Projects/>}/>
+      <Route path="/education" element={<EducationPage/>}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
