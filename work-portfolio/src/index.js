1c4c399d2f375bf9e7b0076e0b67ecc785ab1e8e
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2025-03-02 15:25:43 -0600

    added home page to "/" routes for main landing page

--- work-portfolio/src/index.js
+++ work-portfolio/src/index.js
@@ -6,12 +6,14 @@ import Projects from './pages/projects/index';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from "react-router";
 import EducationPage  from "./pages/education/index";
+import HomePage from './pages/home/index';
 
 const root = document.getElementById('root');
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} >
+      <Route path="/" element={<HomePage/>}/>
       <Route  path="/projects" element={<Projects/>}/>
       <Route path="/education" element={<EducationPage/>}/>
       </Route>
